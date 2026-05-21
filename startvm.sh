1f4d053e6b262ab3010e740ec3db888fc591f47a
Ruizhe Huang <98121925+RRaAy-H@users.noreply.github.com>
2026-05-21 10:23:09 -0400

    Modify QEMU parameters for ARM virtualization

--- startvm.sh
+++ startvm.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 qemu-system-aarch64 \
-  -M virt -cpu max -smp 4 -m 4096 \
+  -accel tcg,thread=multi \
+  -machine virt,gic-version=3 \
+  -cpu max,pauth=off,sve128=on \
+  -smp 36 -m 100000 \
   -drive if=pflash,format=raw,readonly=on,file=vm/QEMU_EFI.fd \
   -drive if=pflash,format=raw,file=vm/QEMU_VARS.fd \
   -drive if=virtio,format=qcow2,file=vm/ubuntu-arm64.qcow2 \
   -drive if=virtio,format=raw,file=vm/seed.iso \
   -netdev user,id=net0,hostfwd=tcp::2222-:22 \
   -device virtio-net-pci,netdev=net0 \
+  -device virtio-rng-pci \
   -nographic
